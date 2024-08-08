aadd02d20f180fefc80562bdfbbed9f69bb8bcd4
Roel <roelhoogendoorn01369@gmail.com>
2024-08-08 20:40:34 +0200

    Improved settings menu

--- src/screens/SettingsScreen.jsx
+++ src/screens/SettingsScreen.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { View, Text, Switch, TouchableOpacity } from 'react-native';
+import React, {useContext} from 'react';
+import {View, Text, Switch, TouchableOpacity, Alert} from 'react-native';
 import {DarkModeContext} from "../context/DarkModeContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {useNavigation} from "@react-navigation/native";
@@ -7,7 +7,7 @@ import {FontAwesome} from "@expo/vector-icons";
 
 const SettingsScreen = () => {
     const navigation = useNavigation();
-    const { colorScheme, toggleDarkMode } = useContext(DarkModeContext);
+    const {colorScheme, toggleDarkMode} = useContext(DarkModeContext);
 
     const deleteData = async () => {
         try {
@@ -18,21 +18,43 @@ const SettingsScreen = () => {
         }
     };
 
+    const confirmDelete = () => {
+        Alert.alert(
+            'Confirm Deletion',
+            'Are you sure you want to delete all data? This action cannot be undone.',
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Delete',
+                    style: 'destructive',
+                    onPress: deleteData,
+                },
+            ],
+            { cancelable: true }
+        );
+    };
+
     return (
-        <View className="flex-1 justify-center items-center p-4 bg-bgLight dark:bg-bgDark">
+        <View className="flex-1 p-5 bg-bgLight dark:bg-bgDark">
             <TouchableOpacity className="absolute top-3 left-3 justify-center items-center h-12 w-12 bg-white rounded-3xl" onPress={() => navigation.navigate('Home')}>
-                <FontAwesome name="arrow-left" size={30} />
+                <FontAwesome name="arrow-left" size={30}/>
             </TouchableOpacity>
-            <View className="flex-row justify-between items-center w-full mb-4">
-                <Text className="text-black dark:text-white text-lg">Dark Mode</Text>
-                <Switch
-                    value={colorScheme === 'dark'}
-                    onValueChange={toggleDarkMode}
-                />
+            <View className="flex-1 gap-4 pt-14">
+                <View className="flex-row items-center justify-between p-3 bg-panelLight dark:bg-panelDark rounded-lg">
+                    <Text className="text-black dark:text-white text-lg">Dark Mode</Text>
+                    <Switch
+                        value={colorScheme === 'dark'}
+                        onValueChange={toggleDarkMode}
+                    />
+                </View>
+                <View className="flex-row items-center justify-between p-3 bg-panelLight dark:bg-panelDark rounded-lg">
+                    <Text className="text-black dark:text-white text-lg">Delete All Data</Text>
+                    <TouchableOpacity className="bg-red-500 p-4 rounded-md" onPress={confirmDelete}><Text>DELETE</Text></TouchableOpacity>
+                </View>
             </View>
-            <TouchableOpacity className="bg-red-500 p-4 rounded-md" onPress={deleteData}>
-                <Text className="text-black dark:text-white">Delete Data</Text>
-            </TouchableOpacity>
         </View>
     );
 };
